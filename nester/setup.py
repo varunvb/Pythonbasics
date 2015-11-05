4fcacf2e826ee629b61e7b3be152008ff3357d80
VarunVB <varunvb3@gmail.com>
2015-11-06 00:10:30 +0530

    adding script for basic

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'nester',
+    version      = '1.0.1',
+    py_modules   = ['nester'],
+    author       = 'varunvb',
+    author_email = 'varunvb3@gmail.com',
+    url          = 'https://tesseract809.wordpress.com/',
+    description  = 'A simple printer for nested lists',
+)
---
+++ nester/testnester.py
@@ -0,0 +1,6 @@
+from nester import print_lol
+
+movies = ["shawshank redemption", "morgan freeman", 65, "cast away", ["tom hanks", ["julia roberts", "nicole kidman"]]]
+
+print_lol(movies, 0)
+
