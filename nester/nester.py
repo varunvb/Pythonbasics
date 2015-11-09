41b341c1c4d359a3ac0ddd9d89a6f658848c2fca
VarunVB <varunvb3@gmail.com>
2015-11-09 11:49:57 +0530

    adding script for basic

--- nester/nester.py
+++ nester/nester.py
@@ -1,5 +1,7 @@
 """This is the "nester.py" module for printing lists that may or may not include nested links"""
 import sys
+
+
 def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
     for each_item in the_list:
         if isinstance(each_item, list):
